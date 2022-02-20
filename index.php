f5fa3a70d45ef5847b8fe2f96a8ec594c560365a
Shubham <shubhamkumar.123.sk001@gmail.com>
2022-02-20 13:48:36 +0530

    init

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PG Life</title>
+
+    <?php
+    include "includes/head_links.php";
+    ?>
+    <link href="css/home.css" rel="stylesheet" />
+    <link href="css/property_list.css" rel="stylesheet" />
+</head>
+
+<body>
+    <?php
+    include "includes/header.php";
+    ?>
+    <!--Search-->
+    <div class="banner-container">
+        <h2 class="white pb-3">Happiness per Square Foot</h2>
+
+        <form id="search-form" action="property_list.php" method="GET">
+            <div class="input-group city-search">
+                <input type="text" class="form-control input-city" id='city' name='city' placeholder="Search For PGs" />
+                <div class="input-group-append">
+                    <button type="submit" class="btn btn-secondary">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </div>
+            </div>
+        </form>
+
+    </div>
+    <!-- Hostels list -->
+    <div>
+        <?php
+        $file = file_get_contents('property_list.php');
+        echo $file;
+        ?>
+    </div>
+    <?php
+    include "includes/signup_modal.php";
+    include "includes/login_modal.php";
+    include "includes/footer.php";
+    ?>
+</body>
+
+</html>
